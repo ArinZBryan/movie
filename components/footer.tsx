b7d53fe7a787f6edd12d16023c467e96faab3ddc
ArinZBryan <arinbryan27@Gmail.com>
2023-05-11 11:14:00 +0100

    Removed Unused Actor Page

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,6 +2,6 @@ export function WebFooter() {
 	return <footer style={{position: "fixed", height: '13vh', backgroundColor: 'lightslategray', width: '100vw', bottom: 0,	left: 0,}}>
         <div>Arbritarty Movie Reviews</div>
         <div>Image Credit: Google / IMDB</div>
-        <div>Made By: Arin Bryan, Brady Clark, Vipulya Chandrashekhar Hande</div>
+        <div>Made By: Arin Bryan, Brady Clark, Vipulya Hande</div>
     </footer>
 }
