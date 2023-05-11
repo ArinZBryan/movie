4c4f12edeace895bd5cafe69029e84d09d6e4377
ArinZBryan <arinbryan27@Gmail.com>
2023-05-11 11:07:09 +0100

    Updated footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,7 @@
 export function WebFooter() {
 	return <footer style={{position: "fixed", height: '13vh', backgroundColor: 'lightslategray', width: '100vw', bottom: 0,	left: 0,}}>
-        Footer
+        <div>Arbritarty Movie Reviews</div>
+        <div>Image Credit: Google / IMDB</div>
+        <div>Made By: Arin Bryan, Brady Clark, Vipulya Chandrashekhar Hande</div>
     </footer>
 }
