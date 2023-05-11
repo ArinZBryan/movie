fd91062aac37b27997e085d5e3cc8d4a02383c83
ArinZBryan <arinbryan27@Gmail.com>
2023-05-11 11:22:47 +0100

    Esdits to header and footer

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,6 @@
 export function WebHeader() {
 	return 	<div style = {{height: '18vh', backgroundColor: 'lightslategray', width: '100vw', position: 'absolute', top: 0, left: 0, fontSize: 'xx-large'}}>
-		<h1 style = {{textAlign: 'right', paddingRight: '1rem', paddingTop: '1rem', paddingBottom: '2rem'}}>
+		<h1 style = {{textAlign: 'right', paddingRight: '2rem', paddingTop: '1rem', paddingBottom: '2rem'}}>
 			<a href="/">Movie Reviews</a>
 		</h1>
 		<div style = {{display: 'flex',	flexDirection: 'row', justifyContent: 'space-around'}}> 
