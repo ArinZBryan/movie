20075e49d04c92e9b1248f10fbd9ab6c12a863c9
ArinZBryan <arinbryan27@Gmail.com>
2023-05-11 13:20:14 +0100

    Changes

--- app/movies/mov1/page.tsx
+++ app/movies/mov1/page.tsx
@@ -31,7 +31,10 @@ export default function Home() {
 		["Andrew O'Hagan (Daily Telegraph UK)", "Here is a movie of the times, funny, enjoyable, perfect-looking, and altogether original in a way that might cause us to look again at the meaning of the word."],
 		["Chris Tookey (Daily Mail)", "Adults will find plenty to laugh at. Children will love it"],
 		["Daily News", "An adorable, infectious work of true sophistication"],
-		["Chicago Sun Times", "This is not your average family cartoon. Shrek is jolly and wicked, filled with sly in-jokes and yet somehow possessing a heart"]
+		["Chicago Sun Times", "This is not your average family cartoon. Shrek is jolly and wicked, filled with sly in-jokes and yet somehow possessing a heart"],
+		["planktonrules21", "For younger viewers, they might not understand the impact of 'Shrek' on the film industry and CGI movies. However, being an older guy, I remember how groundbreaking and amazing the computer animation was when the film debuted. It was head and shoulders better than the few CGI movies that had come out in the previous decade...and it finally gave Pixar a serious run for the money. In addition, the plot was very adult...and the film's appeal was much g..."],
+		["smashattack8", "Shrek is completely new. Never has a fully CGI movie taken on the twists, turns, and surprises as Shrek has. It's a full parody of numerous fairytales, and its humor is, well, humor at its best. Grown men will laugh at the jokes of Farquaad's Stature and the hidden meaning behind every little comment. Of course Shrek, like all other movies, has its emotional moments (so what if I lied about it being completely new?)."],
+		["rhirahan29", "A movie suitable for children, but really made for an adult audience. It's funny, has many quotable catchphrases and teaches us so much. It shows us that love has no limits, that you can find love where you'd least expect it and that beauty doesn't show only on the outside. It is also a great story about unlikely characters forming friendships that will last a lifetime."],
 	]
 	const statistics = [
 		["Metacritic", "4/5"],
--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,9 @@
 export function WebFooter() {
-	return <footer style={{position: "fixed", height: '10vh', backgroundColor: 'lightslategray', width: '100vw', bottom: 0,	left: 0, padding: "10px"}}>
+	return <footer style={{position: "fixed", height: '12vh', backgroundColor: 'lightslategray', width: '100vw', bottom: 0,	left: 0, padding: "10px"}}>
         <div>Arbritarty Movie Reviews</div>
         <div>Image Credit: Google / IMDB</div>
         <div>Made By: Arin Bryan, Brady Clark, Vipulya Hande</div>
+        <div style={{display:"flex", flexDirection:"row"}}>Made With:  <img src="/next.svg" className="footerImg" style={{height:"1em", paddingLeft:"0.5em"}}></img></div>
     </footer>
 }
+
